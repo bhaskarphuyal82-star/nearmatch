0b5785c8a43b74a6c2261888d5dd9606377cffd3
aasish limbu <146429145+aasishdeveloper@users.noreply.github.com>
2026-01-10 00:24:05 +0545

    feat: apply comprehensive robots.txt rules for various crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -7,18 +7,63 @@ export default async function robots(): Promise<MetadataRoute.Robots> {
     const config = await getSiteConfig();
     const baseUrl = config.siteUrl || 'https://nearmatch.site';
 
+    const commonDisallows = [
+        '/connections/',
+        '/forgot_enter.phtml',
+        '*/billing/allopass_unsubscribe.phtml',
+        '/email*',
+        '/*/email*',
+        '/pin/',
+        '/access-token/', // mobile
+        '/new-password/', // mobile
+        '/a/', // mobile
+        '/w/', // mobile
+        '*/too_young.phtml',
+        '*/profile/*',
+    ];
+
     return {
         rules: [
             {
-                userAgent: '*',
-                allow: '/',
-                disallow: ['/admin/', '/api/', '/_next/'],
+                userAgent: 'MJ12bot',
+                disallow: '',
             },
             {
-                userAgent: 'Mediapartners-Google',
-                allow: '/',
-            }
+                userAgent: 'Googlebot',
+                allow: '/access.phtml',
+                disallow: [
+                    ...commonDisallows,
+                    '*/page-',
+                ],
+            },
+            {
+                userAgent: ['Mediapartners-Google', 'AdsBot-Google', 'AdsBot-Google-Mobile'],
+                disallow: '',
+            },
+            {
+                userAgent: 'msnbot',
+                allow: '/access.phtml',
+                disallow: commonDisallows,
+            },
+            {
+                userAgent: 'Yandex',
+                disallow: [
+                    '/access.phtml',
+                    ...commonDisallows,
+                ],
+            },
+            {
+                userAgent: '*',
+                disallow: [
+                    '/admin/',
+                    '/api/',
+                    '/_next/',
+                    '/access.phtml',
+                    ...commonDisallows,
+                ],
+            },
         ],
         sitemap: `${baseUrl}/sitemap.xml`,
+        host: baseUrl,
     };
 }
